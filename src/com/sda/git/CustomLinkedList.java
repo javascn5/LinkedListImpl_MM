9467593e5f3a10ea56db57e5991b17f242ba9e49
Mates <marczak.mat@gmail.com>
2018-10-21 13:38:33 +0200

    implementacja interfejsu

---
+++ src/com/sda/git/CustomLinkedList.java
@@ -0,0 +1,19 @@
+package com.sda.git;
+
+public class CustomLinkedList<E> implements CustomList<E> {
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public void add(E e) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
--- src/com/sda/git/Main.java
+++ src/com/sda/git/Main.java
@@ -1,8 +1,25 @@
 package com.sda.git;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
+        // Arraylist - implementacja tablicowa
+        // LinkedList - implementacja 
+
+        List<String> list = new LinkedList<>();
+
+        list.add("Jan");
+        list.add("Marczin");
+        list.add("Karol");
+
+        for(String l: list)
+            System.out.println(l);
+
+
     }
 }
